7e7143e6c39035b0f55fb6652ffaba547ad13da9
sophiehalish <shalish@elon.edu>
2022-10-28 16:21:29 -0400

    Changes to Enemy in order to get it to move

--- src/galaga/Enemy.java
+++ src/galaga/Enemy.java
@@ -1,17 +1,24 @@
 package galaga;
 
 import highLevel.Breakables;
+import javafx.geometry.Point2D;
 
 public class Enemy extends Breakables {
 
     private final int pointValue=1;
     private int amountToBreak;
 
+    private Point2D enemyVelocity;
+
+    private static final int ENEMY_SPEED=10;
+
     public static final String ENEMY_IMAGE="resources/";
 
     public Enemy(){
         super(ENEMY_IMAGE);
         amountToBreak=1;
+        // turn speed into velocity that can be updated on bounces
+        enemyVelocity = new Point2D(ENEMY_SPEED,ENEMY_SPEED);
     }
 
     @Override
@@ -24,8 +31,20 @@ public class Enemy extends Breakables {
         return pointValue;
     }
 
+    @Override
+    //for now, same as brick but will most likely change
+    public void setStartLocation(int currentBrick, int numBricks) {
+        breakableView.setX(BRICK_WIDTH*currentBrick);
+        breakableView.setY(BRICK_HEIGHT*currentBrick);
+    }
+
     public void enemyHit()
     {
         amountToBreak--;
+    }
+
+    //Want the enemies to slowly move down the screen, so only Y value changes
+    public void move(double elapsedTime){
+        breakableView.setY(breakableView.getY() + enemyVelocity.getY() * elapsedTime*-1);
     }
 }
