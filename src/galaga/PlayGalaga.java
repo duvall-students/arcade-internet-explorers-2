e7a8d2b3d412621403a7bd514e11c87fea0869ce
madelinevolchko <madelinevolchko@madelines-air.elon.edu>
2022-10-31 15:31:04 -0400

    working on step xoxo

--- src/galaga/PlayGalaga.java
+++ src/galaga/PlayGalaga.java
@@ -3,7 +3,9 @@ package galaga;
 import java.util.ArrayList;
 import java.util.List;
 
+import breakout.BlockBrick;
 import breakout.Brick;
+import highLevel.Breakables;
 import highLevel.Player;
 import highLevel.VideoGame;
 import javafx.animation.KeyFrame;
@@ -98,6 +100,101 @@ public class PlayGalaga extends Application implements VideoGame {
 			return scene;
 		}
 		
+		// Change properties of shapes in small ways to animate them over time
+	    public void step (double elapsedTime) {
+	    	
+	        // updated the lasers
+	    	for(Laser thisLaser : myLasers) {
+	    		thisLaser.move(elapsedTime);
+	    	}
+			
+	        // check for collision
+	        // collision(1) means it hit brick, collision(0) means it hit paddle
+			for (Laser thisLaser : myLasers) {
+				for (Enemy thisEnemy : myEnemies) {
+					if (thisLaser.getView().getBoundsInParent().intersects(thisEnemy.getView().getBoundsInParent())) {
+						
+					}
+				}
+				if ( thisLaser.getView().getBoundsInParent().intersects(brick.getView().getBoundsInParent())) {
+					theBall.collision();
+					brickCollision(brick);
+					
+					//print it
+					Text text2=new Text();
+			    	String currentPoints="Current Points: "+CURRENTSCORE;
+			    	text2.setText(currentPoints);
+			    	text2.setX(10);
+			    	text2.setY(370);
+			    	root.getChildren().set(1,text2);
+				}
+			}
+			for(BlockBrick wall : myBlocks) {
+				if ( theBall.getView().getBoundsInParent().intersects(wall.getView().getBoundsInParent())){
+					theBall.collision();
+				}
+			}
+
+			for (Breakables powerUp : myPowerUps)
+			{
+				if(theBall.getView().getBoundsInParent().intersects(powerUp.getView().getBoundsInParent()))
+				{
+					myPowerUps.remove(powerUp);
+					root.getChildren().remove(powerUp.getView());
+					//increase point multiplier
+					POINTMULTIPLIER++;
+				}
+			}
+			if (myBricks.isEmpty()) {
+//				onLevelOne=false;
+				level++;
+				//message about new level
+				Text restart=new Text();
+				String message="Level Complete \n To start next level, click anywhere on the screen";
+				restart.setText(message);
+				restart.setX(100);
+				restart.setY(100);
+				root.getChildren().add(restart);
+				advanceLevel(level);
+				//locationOfMessage=root.getChildren().indexOf(restart);
+				root.getChildren().remove(restart);
+				//restart if clicked	
+			}
+			
+			if ( theBall.getView().getBoundsInParent().intersects(userPaddle.getView().getBoundsInParent()) ) {
+				theBall.collision();
+			}
+			
+			//this is not the correct bounds, but basically if it goes past a certain boundry point, I just dont know what H means
+			if (!bounceVariable) {
+				//resets both and bounces
+				userPaddle.resetPaddle();
+				theBall.resetBall();
+				theBall.collision();
+				// player loses life. Problem is that Points/Player is not in this package
+				myPlayer.loseLife();
+				PLAYERLIVES=myPlayer.getLifeAmount();
+				Text text=new Text();
+		    	String welcome="Lives Left: "+PLAYERLIVES;
+		    	text.setText(welcome);
+		    	text.setX(10);
+		    	text.setY(390);
+				root.getChildren().set(0, text);
+				
+			}
+			
+			if (myPlayer.getLifeAmount()==0) {
+				root.getChildren().clear();
+				Text endMessage=new Text();
+				String message="You Lost :( \n Your point value was "+myPlayer.getCurrentScore();
+				endMessage.setText(message);
+				endMessage.setX(150);
+				endMessage.setY(150);
+				root.getChildren().add(endMessage);
+			}
+		}
+
+		
 		public void setUpBreakable(int level){
 			//adding in however many bricks wanted
 			for(int i=0;i<ENEMYAMOUNT;i++) {
@@ -119,7 +216,10 @@ public class PlayGalaga extends Application implements VideoGame {
 			}
 			if (code== KeyCode.SPACE) {
 				//need a shooting code for laser
-				myLaser.shoot();
+				Laser newLaser=new Laser();
+				newLaser.setStartLocation(mySpaceship.getView().getX(), mySpaceship.getView().getY());
+				myLasers.add(newLaser);
+				root.getChildren().add(newLaser.getView());
 			}
 		}
 		
